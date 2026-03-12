c9ea459de2b4a58ae018d7579af44bfaa95fb800
zhangpulin <zhangpulin@shpubing.com>
2026-03-12 19:22:25 +0800

    delete default

--- open-next.config.ts
+++ open-next.config.ts
@@ -2,8 +2,5 @@ import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 //import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 
 export default defineCloudflareConfig({
-  default: {
-    runtime: "nodejs", // 必须设为 nodejs 以兼容你的 PG 驱动
-  },
  // incrementalCache: r2IncrementalCache,
 });
